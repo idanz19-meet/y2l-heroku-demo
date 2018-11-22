14656a68b99efe96118bdeebb53f18491bd717c6
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:06:00 +0200

    dddd

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World test!'
+    return 'The modern bourgeois society that has sprouted from the ruins of feudal society has not done away with class antagonisms. It has but established new classes, new conditions of oppression, new forms of struggle in place of the old ones.'
 
 if __name__ == '__main__':
     app.run(debug=True)
